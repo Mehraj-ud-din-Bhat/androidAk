d99eceb0ad7e3494c56c5ee4f8e8a08715ef2466
mehraj <mehraj@cdse-it.com>
2022-05-11 09:07:06 +0530

    using intent

--- app/src/main/java/com/mehraj/MainActivity.java
+++ app/src/main/java/com/mehraj/MainActivity.java
@@ -3,12 +3,15 @@ package com.mehraj;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,47 +20,20 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-   EditText editTextNumber;
-   Button buttonCheck;
-   TextView display;
+     Button movebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-     editTextNumber=findViewById(R.id.number);
-     buttonCheck=findViewById(R.id.btn_check);
-     display=findViewById(R.id.display);
-
-    buttonCheck.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            if(editTextNumber.getText().toString().isEmpty())
-            {
-                editTextNumber.setError("Required");
-
-                return;
+        movebtn=findViewById(R.id.MOVE);
+        movebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
             }
-
-            Integer number=Integer.parseInt(editTextNumber.getText().toString());
-
-
-            if(number%2==0)
-            {
-                display.setText("You have entered an even number");
-            }else {
-                display.setText("You have entered an odd number");
-            }
-
-
-
-
-        }
-    });
-
-
-
+        });
 
 
 
