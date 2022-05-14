bff8f7cc8accb2b690a77547bcdb847ce4856f76
mehraj <mehraj@cdse-it.com>
2022-05-14 08:31:01 +0530

    using intent and data

--- app/src/main/java/com/mehraj/SecondActivity.java
+++ app/src/main/java/com/mehraj/SecondActivity.java
@@ -2,6 +2,7 @@ package com.mehraj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -14,10 +15,13 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         display=findViewById(R.id.display);
-        String name=getIntent().getStringExtra("myname");
-        String phone=getIntent().getStringExtra("myphone");
 
-        display.setText("this data was passed by mainactivity\n Name: "+name+"\nPhone: "+phone);
+        Intent intent=getIntent();
+
+        String name=intent.getStringExtra("myname");
+        String phone=intent.getStringExtra("myphone");
+
+        display.setText("This data was passed by mainactivity\n Name: "+name+"\nPhone: "+phone);
 
 
     }
