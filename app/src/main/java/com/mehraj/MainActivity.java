04543796563ddaae833b33f9e06ee23403808233
mehraj <mehraj@cdse-it.com>
2022-05-25 07:40:52 +0530

    using relative layout

--- app/src/main/java/com/mehraj/MainActivity.java
+++ app/src/main/java/com/mehraj/MainActivity.java
@@ -27,39 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        submit=findViewById(R.id.submit);
-        name=findViewById(R.id.name);
-        phone=findViewById(R.id.phone);
 
-       submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                /**
-                 *   EXPLICIT INTENT
-                 *
-                 */
-
-                if(name.getText().toString().isEmpty())
-                {
-                    name.setError("Please enter your name");
-                    return;
-                }
-
-                if(phone.getText().toString().isEmpty())
-                {
-                    phone.setError("Please enter your phone");
-                    return;
-                }
-
-
-                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
-                intent.putExtra("myname",name.getText().toString());
-                intent.putExtra("myphone",phone.getText().toString());
-
-                startActivity(intent);
-            }
-        });
 
 
 
